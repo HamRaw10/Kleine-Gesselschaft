22c7f2f838dd24e2825b1fb77ae8a68989ff984b
Maximo Gonzalez <maximo.gonzalez186@gmail.com>
2025-07-02 17:53:06 -0300

    Se han incrementado 2 nuevos cambios, primero se logro que el circulo se mueva gradualmente segun el clickeo del raton en la pantalla, y segundo se incorporaron limites del circulo en la pantalla, permitiendo que el circulo no desaparezca, ademas se agregaron los cambios en el change.log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,9 @@ Este archivo documenta los cambios importantes realizados en el proyecto a lo la
 
 ### Changed
 - Se agrego un circulo que proximamente representara un personaje que se puede mover con teclas y con clicks en pantalla.
+- Se agrego movimiento para ese circulo, en el cual el mismo se mueve hacia donde el mouse lo indique, de forma gradual, al 
+clickear en algun lugar de la pantalla, ese ciruclo se va a dirigir gradualmente ahi, ademas se agregaron limites para el circulo, dentro de la pantalla.
+
 
 ### Fixed
 - No se ha arreglado nada.
--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -17,18 +17,26 @@ public class Main extends ApplicationAdapter {
 
     float circleX = 200;
     float circleY = 100;
+    float circleRadius = 75;
 
     @Override
     public void create() {
         shapeRenderer = new ShapeRenderer();
     }
 
+
+
     @Override
     public void render() {
 
-        if(Gdx.input.isTouched()){
-            circleX = Gdx.input.getX();
-            circleY = Gdx.graphics.getHeight() - Gdx.input.getY();
+        if(Gdx.input.isTouched()){//Para el mouse
+            float velocidad = 0.03f;//Variable creada, la cual va a almacenar la velocidad en la que se mueva el ciruclo
+            float targetX = Gdx.input.getX();//Variable para indicar que pertenece a x
+            float targetY = Gdx.graphics.getHeight() - Gdx.input.getY();//Variable para indicar que pertenece a Y
+            circleX += (targetX - circleX) * velocidad * Gdx.graphics.getDeltaTime() * 60;;//Variable del circulo en el eje x donde indica que el circulo se va a dirigir donde vaya el mouse
+            circleY += (targetY - circleY) * velocidad * Gdx.graphics.getDeltaTime() * 60;;//Variable del circulo en el eje y donde indica que el circulo se va a dirigir donde vaya el mouse
+            if (Math.abs(targetX - circleX) < 0.5f) circleX = targetX;
+            if (Math.abs(targetY - circleY) < 0.5f) circleY = targetY;
         }
 
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
@@ -44,6 +52,14 @@ public class Main extends ApplicationAdapter {
             circleX++;
         }
 
+        float minX = circleRadius;//
+        float maxX = Gdx.graphics.getWidth() - circleRadius;//
+        float minY = circleRadius;//
+        float maxY = Gdx.graphics.getHeight() - circleRadius;//
+
+        circleX = Math.min(Math.max(circleX, minX), maxX);//
+        circleY = Math.min(Math.max(circleY, minY), maxY);//
+
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -58,3 +74,12 @@ public class Main extends ApplicationAdapter {
         shapeRenderer.dispose();
     }
 }
+
+
+
+
+
+
+
+
+
