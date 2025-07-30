8e820c17b6199128e157f9444a3027e2cd92b506
fabri <fabriziodrudaz@gmail.com>
2025-07-30 14:57:16 -0300

    importacion del sprite batch

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -6,6 +6,7 @@ import pantalla.PantallaCarga;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
